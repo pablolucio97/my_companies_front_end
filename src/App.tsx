551dd09ecf0252e70455bb079c240a98cc796d89
pablolucio97 <pablolucio_@hotmail.com>
2023-05-11 16:10:11 -0300

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <p>Hello from My Companies</p>
+    </>
+  )
+}
+
+export default App
---
+++ src/theme/index.ts
@@ -0,0 +1,53 @@
+const theme = {
+    title: 'default',
+    colors: {
+        primary: '#0385FD',
+        secondary: '#00CC99',
+        text: '#000000',
+        title: '#4D4D4D',
+        label: '#373737',
+        backgroundLight: '#FFFFFF',
+        backgroundGrey: '#EAEAEA',
+        background: '#575656',
+        success: '#079904',
+        error: '#C90808',
+    },
+    fonts: {
+        primary_300: 'Poppins_300Light',
+        primary_400: 'Poppins_400Regular',
+        primary_500: 'Poppins_500Medium',
+        primary_600: 'Poppins_600SemiBold',
+        primary_700: 'Poppins_700Bold',
+        primary_800: 'Poppins_800ExtraBold',
+    },
+    spacings: [
+        4,
+        8,
+        12,
+        16,
+        20,
+        24,
+        32,
+        40,
+        48,
+        56
+    ],
+    sizes: [
+        10,
+        12,
+        14,
+        16,
+        20,
+        24,
+        32,
+        40,
+        48,
+        56,
+        64,
+        72,
+        80,
+        120
+    ]
+}
+
+export default theme
